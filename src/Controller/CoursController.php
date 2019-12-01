b1dea1df86b7a434906a79ca26e0c946f2b6a66a
Loic Auge <35797273+LoicAuge@users.noreply.github.com>
2019-12-01 16:27:20 +0100

    Informations BDD page Month

--- src/Controller/CoursController.php
+++ src/Controller/CoursController.php
@@ -83,49 +83,7 @@ class CoursController extends AbstractController
     function afficherCreneauMois($noann, $nomon){
         if ($nomon >=1 && $nomon<=12) {
             $jour = mktime(0, 0, 0, $nomon, 1, $noann);
-            $nb = 0;
-            $jourASauter = 0;
-            $sem = date("W", $jour);
-            $nbSem = $this->weeksPerMonth($nomon, $noann);
-            $tabJours = array(array(), array(), array(), array(), array(), array());
-            if (date("N", $jour) < 6) {
-                array_push($tabJours[$nb], "Semaine " . $sem);
-            } else {
-                switch (date("N", $jour)) {
-                    case 6:
-                        $jour += 86400 * 2;
-                        $sem++;
-                        array_push($tabJours[$nb], "Semaine " . $sem);
-                        $jourASauter = 2;
-                        break;
-                    case 7:
-                        $jour += 86400;
-                        $sem++;
-                        array_push($tabJours[$nb], "Semaine " . $sem);
-                        $jourASauter = 1;
-                        break;
-                }
-            }
-            for ($j = 1; $j < date("N", $jour); $j++) {
-                array_push($tabJours[$nb], " ");
-            }
-            $nbjour = date("t", $jour);
-            for ($j = 1; $j <= $nbjour - $jourASauter; $j++) {
-                if (date("W", $jour) != $sem) {
-                    $nb = $nb + 1;
-                    $sem = date("W", $jour);
-                    array_push($tabJours[$nb], "Semaine " . $sem);
-                }
-                if (date("N", $jour) < 6) {
-                    array_push($tabJours[$nb], date("j", $jour) . " " . $this->deterMois(date("n", $jour)) . " " . date("Y", $jour));
-                }
-                $jour += 86400;
-            }
-            for ($nbTab = $nb; $nbTab < 6; $nbTab++) {
-                for ($posTab = count($tabJours[$nbTab]); $posTab < 6; $posTab++) {
-                    array_push($tabJours[$nbTab], " ");
-                }
-            }
+            $tabJours = $this->setDaysMonth($nomon, $noann);
             $this->console_log($tabJours);
             return $this->render('/cours/month.html.twig', ['tabJours' => $tabJours, 'nomon' => $nomon, 'noann' => $noann]);
         }elseif($nomon == 13 || $nomon == 0) {
@@ -301,6 +259,98 @@ class CoursController extends AbstractController
         else $res = (int)$nweek;
         return $res + 1;
     }
+
+    function setDaysMonth($nomon, $noann){
+        $jour = mktime(0, 0, 0, $nomon, 1, $noann);
+        $nb = 0;
+        $jourASauter = 0;
+        $sem = date("W", $jour);
+        $nbSem = $this->weeksPerMonth($nomon, $noann);
+        $tabJours = array(array(), array(), array(), array(), array(), array());
+        if (date("N", $jour) < 6) {
+            array_push($tabJours[$nb], array("Semaine " . $sem," "));
+        } else {
+            switch (date("N", $jour)) {
+                case 6:
+                    $jour += 86400 * 2;
+                    $sem++;
+                    array_push($tabJours[$nb], array("Semaine " . $sem," "));
+                    $jourASauter = 2;
+                    break;
+                case 7:
+                    $jour += 86400;
+                    $sem++;
+                    array_push($tabJours[$nb], array("Semaine " . $sem," "));
+                    $jourASauter = 1;
+                    break;
+            }
+        }
+        for ($j = 1; $j < date("N", $jour); $j++) {
+            array_push($tabJours[$nb], array(" "," "));
+        }
+        $nbjour = date("t", $jour);
+        for ($j = 1; $j <= $nbjour - $jourASauter; $j++) {
+            if (date("W", $jour) != $sem) {
+                $nb = $nb + 1;
+                $sem = date("W", $jour);
+                array_push($tabJours[$nb], array("Semaine " . $sem," "));
+            }
+            if (date("N", $jour) < 6) {
+                //array_push($tabJours[$nb], date("j", $jour) . " " . $this->deterMois(date("n", $jour)) . " " . date("Y", $jour));
+                $query1 = $this->entityManager->createQuery('SELECT m.intitule AS intitule FROM App\Entity\Matiere m, App\Entity\Cours c
+                        WHERE m.id = c.fk_matiere_id and c.fk_intervenant_id = 1 and c.debut >= ' . date('Ymd', $jour) . ' AND c.debut < ' . date('Ymd', $jour + 86400) . ' ORDER BY c.debut');
+                $tabRetQuery = $query1->getResult();
+                if (count($tabRetQuery) >0) {
+                    if ($tabRetQuery[0]['intitule'] == "ENTREPRISE") {
+                        array_push($tabJours[$nb], array(date("j", $jour) . " " . $this->deterMois(date("n", $jour)) . " " . date("Y", $jour), "E"));
+                    } else {
+                        array_push($tabJours[$nb], array(date("j", $jour) . " " . $this->deterMois(date("n", $jour)) . " " . date("Y", $jour), "C"));
+                    }
+                }else {
+                    array_push($tabJours[$nb], array(date("j", $jour) . " " . $this->deterMois(date("n", $jour)) . " " . date("Y", $jour), " "));
+                }
+            }
+            $jour += 86400;
+        }
+        for ($nbTab = $nb; $nbTab < 6; $nbTab++) {
+            for ($posTab = count($tabJours[$nbTab]); $posTab < 6; $posTab++) {
+                array_push($tabJours[$nbTab], array(" "," "));
+            }
+        }
+        return $tabJours;
+    }
+
+    /*function setTabsMonth($jour)
+    {
+        $nb = 0;
+        $tabRet = array(array(),array(),array(),array(),array(),array());
+        if(date("N", $jour)>5){
+            switch (date("N", $jour)) {
+                case 6:
+                    $jour += 86400 * 2;
+                    $jourASauter = 2;
+                    break;
+                case 7:
+                    $jour += 86400;
+                    $jourASauter = 1;
+                    break;
+            }
+        }
+        for ($j = 1; $j < date("N", $jour); $j++) {
+            array_push($tabRet[$nb], " ");
+        }
+        for ($i = 0; $i <= 4; $i++) {
+            $query1 = $this->entityManager->createQuery('SELECT m.intitule AS intitule FROM App\Entity\Matiere m, App\Entity\Cours c
+                        WHERE m.id = c.fk_matiere_id and c.fk_intervenant_id = 1 and c.debut >= ' . date('Ymd', $jour) . ' AND c.debut < ' . date('Ymd', $jour + 86400) . ' ORDER BY c.debut');
+            $tabRetQuery = $query1->getResult();
+            if ($tabRetQuery[0]['intitule'] == "ENTREPRISE"){
+                array_push($tabRet,"E");
+            }else {
+                array_push($tabRet,"C");
+            }
+        }
+        return $tabRet;
+    }*/
 
 
 }
